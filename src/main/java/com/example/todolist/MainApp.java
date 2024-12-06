5bb2bd417499ac4aab29d6b9204241644ef401ff
ilyes izmr <i.izemmouren@gmail.com>
2024-12-06 23:06:28 +0100

    ive connect the log in and sign up pages to the home page

--- src/main/java/com/example/todolist/MainApp.java
+++ src/main/java/com/example/todolist/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         // Load the FXML file for the sign-up view
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/todolist/view/fxml/LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/todolist/view/fxml/HomeView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         // Set up the stage (main window)
