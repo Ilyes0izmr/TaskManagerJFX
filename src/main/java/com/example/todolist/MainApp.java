dfc379736e4d60be30097044b78a2ae87144e60c
ilyes izmr <i.izemmouren@gmail.com>
2024-12-06 01:47:42 +0100

    i added the View aka css-fxml , for the log in/sign up pages

--- src/main/java/com/example/todolist/MainApp.java
+++ src/main/java/com/example/todolist/MainApp.java
@@ -12,18 +12,25 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         // Load the FXML file for the login view
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/fxml/LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/fxml/LogInView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         // Set up the stage (main window)
         primaryStage.setTitle("To-Do List Application");
         primaryStage.setScene(scene);
+
+        //Optional: set initial window size (adjust as needed)
+        primaryStage.setWidth(1550);   // Set the width of the window
+        primaryStage.setHeight(850);  // Set the height of the window
+
+        // Optional: make the window resizable or not
+        primaryStage.setResizable(true);
+
+        // Show the window
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(); // Launch the JavaFX application
-        //test test test test test
-
     }
 }
