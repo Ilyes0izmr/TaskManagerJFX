0d181d715f6c10daf5083c8593e824d0d7b9ad41
ilyes izmr <i.izemmouren@gmail.com>
2024-12-07 02:20:59 +0100

    rani ray7 nr9d n3l rastk ya lkey

---
+++ src/main/java/com/example/todolist/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.controller.LoginController;
+import com.example.todolist.model.Priority;
+import com.example.todolist.model.Reminder;
+import com.example.todolist.model.Status;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+
+public class HomeController {
+
+    @FXML
+    private ComboBox<String> taskCategoryComboBox;
+
+    @FXML
+    private TextField taskFieldDescription;
+
+    @FXML
+    private DatePicker taskFieldDueDate;
+
+    @FXML
+    private TextField taskFieldTitle;
+
+    @FXML
+    private ComboBox<Priority> taskPriorityComboBox;
+
+    @FXML
+    private ComboBox<Reminder> taskReminderComboBox;
+
+
+    public void handleAddTask(){
+        String taskTitle = taskFieldTitle.getText();
+        String taskDescription = taskFieldDescription.getText();
+        Status status = Status.PENDING ;
+        LocalDate deuDate = taskFieldDueDate.getValue();
+        LocalDate creationDate = LocalDate.now();
+        Priority priority = taskPriorityComboBox.getValue();
+        Reminder reminder = taskReminderComboBox.getValue();
+        String categoryName = taskCategoryComboBox.getValue();
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/todolist/controller/TaskController.java
@@ -0,0 +1,17 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.controller.LoginController;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class TaskController {
+    public AnchorPane background;
+    //private TextField
+
+    public void handleAddTask(){
+
+
+
+
+    }
+}
